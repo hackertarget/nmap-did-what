dcb00f9aab8d2af9fbc8ad74039f51227708a2ec
Peter <peter@hackertarget.com>
2024-05-20 11:47:05 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# nmap-did-what
+
+**nmap-did-what** is a Grafana docker container and a Python script to parse Nmap XML output to an SQLite database. The SQLite database is used as a datasource within Grafana to view the Nmap scan details in a dashboard.
+
+## Overview
+
+The project consists of two main components:
+
+1. A Python script that parses Nmap XML output and stores the data in an SQLite database.
+2. A Grafana Docker container with a pre-configured dashboard for visualizing the Nmap scan data.
+
+### File Structure
+
+- **nmap-to-sqlite.py**: A Python script that parses Nmap XML output and stores the data in an SQLite database.
+- **Dockerfile**: Creates a Docker container based on the official Grafana image, including the necessary configurations for the SQLite data source and dashboard.
+- **docker-compose.yml**: A Docker Compose file that sets up the Grafana container, configuring it to use the SQLite database and including volumes for persistent storage and configuration.
+- **dashboard.yml**: A configuration file that specifies the dashboard provider settings for Grafana.
+- **datasource.yml**: Configures Grafana to use the SQLite database containing the Nmap scan data as the data source.
+- **/data/nmap_results.db**: location in container for the SQLite DB.
+
+## Usage
+
+To get started with nmap-did-what, ensure you have Docker and Docker Compose installed on your system.
+
+Follow these steps to deploy the environment:
+
+1. **Clone the repository**
+
+```
+git clone https://github.com/yourusername/nmap-did-what.git
+```
+
+2. **Parse Nmap XML output**
+
+Run the `nmap-to-sqlite.py` script to parse your Nmap XML output and store the data in an SQLite database:
+
+```
+cd nmap-did-what/data/
+python nmap-to-sqlite.py nmap_output.xml
+```
+
+3. **Start the Grafana Container**
+
+Use Docker Compose to start the Grafana container:
+
+```
+cd nmap-did-what
+docker-compose up -d
+```
+
+4. **Access Grafana**
+
+Once the container is up and running, access the Grafana dashboard through your web browser:
+
+```
+http://localhost:3000
+```
+
+Use the default Grafana credentials (admin/admin) unless changed in the configuration. The Nmap dashboard should be loaded with the data from your Nmap scans.
+
+Multiple scans can be reviewed within the DB and the Nmap Dashboard time filters can be used to the view the scan information based on the time stamps from the scans.
+
+## Customization
+
+- Modify the `nmap-to-sqlite.py` script to extract additional information from the Nmap XML output or to change the structure of the SQLite database.
+- Custom Dashboard are easy to implement, simply adjust the Grafana dashboard to your requirements. Export the JSON of the Dashboard and replace the default Dashboard or create additional dashboard. The ability to spin up a Grafana Docker container with a prebuilt Dashboard is a nice feature.
+- Automation is possible, as you can simply run **nmap** with a cron job, parse the XML with **nmap-to-sqlite.py** and the updated DB will have the newly acquired scan information.
+
+## Credits
+
+Thanks to the Nmap and Grafana projects for providing powerful open-source tools for network scanning and data visualization.
---
+++ data/nmap-to-sqlite.py
@@ -0,0 +1,220 @@
+import xml.etree.ElementTree as ET
+import sqlite3
+from datetime import datetime
+import argparse
+
+#
+# Script to parse nmap xml files and populate an SQLite DB
+# use with Grafana Dashboard - https://hackertarget.com/nmap-dashboard-with-grafana/
+#
+
+def parse_nmap_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    nmap_version = root.get('version', '')
+    command_line = root.get('args', '')
+
+    scan_start_time = root.get('start')
+    if scan_start_time is not None:
+        # timestamps set to match native grafana format
+        scan_start_timestamp = int(scan_start_time) * 1000
+
+    elapsed_time = ''
+    elapsed_time_elem = root.find('runstats/finished')
+    if elapsed_time_elem is not None:
+        elapsed_time = elapsed_time_elem.get('elapsed')
+
+
+    total_hosts = 0
+    total_open_ports = 0
+
+    hosts = []
+    for host in root.findall('host'):
+        total_hosts += 1
+        ip = host.find('address').get('addr', '')
+        
+        hostname_elems = host.findall('hostnames/hostname')
+        hostname = hostname_elems[0].get('name', '') if hostname_elems else ''
+        
+        os = 'Unknown'
+        os_element = host.find('os')
+        if os_element:
+            os_match = os_element.find('osmatch')
+            os = os_match.get('name', 'Unknown') if os_match else 'Unknown'
+        
+        ports_tested = 0
+        ports_open = 0
+        ports_closed = 0
+        ports_filtered = 0
+
+        ports = []
+        for port in host.find('ports').findall('port'):
+            port_id = port.get('portid')
+            protocol = port.get('protocol')
+            state = port.find('state').get('state')
+            if state == 'open':
+                ports_open += 1
+                total_open_ports += 1
+            elif state == 'closed':
+                ports_closed += 1
+            elif state == 'filtered':
+                ports_filtered += 1
+
+            service = port.find('service')
+            service_name = service.get('name', None) if service else None
+            service_product = service.get('product', None) if service else None
+            service_version = service.get('version', None) if service else None
+            service_ostype = service.get('ostype', None) if service else None
+            service_info = (service_product if service_product else '') + (' ' + service_version if service_version else '')
+            http_title = None
+            ssl_common_name = None
+            ssl_issuer = None
+
+            scripts = port.findall('script')
+            for script in scripts:
+                if script.get('id') == 'http-title':
+                    http_title = script.get('output')
+                elif script.get('id') == 'ssl-cert':
+                    for table in script.findall('table'):
+                        if table.get('key') == 'subject':
+                            cn_elem = table.find("elem[@key='commonName']")
+                            if cn_elem is not None:
+                                ssl_common_name = cn_elem.text
+                        elif table.get('key') == 'issuer':
+                            issuer_elems = {elem.get('key'): elem.text for elem in table.findall('elem')}
+                            if 'commonName' in issuer_elems:
+                                ssl_issuer = f"{issuer_elems.get('commonName')} {issuer_elems.get('organizationName', '')}".strip()
+
+            if service_ostype and os == 'Unknown':
+                os = service_ostype
+            
+            ports.append({
+                'port': port_id,
+                'protocol': protocol,
+                'state': state,
+                'service_name': service_name,
+                'service_info': service_info,
+                'http_title': http_title,
+                'ssl_common_name': ssl_common_name,
+                'ssl_issuer': ssl_issuer
+            })
+
+        extraports = host.find('ports').find('extraports')
+        if extraports:
+            extraports_count = int(extraports.get('count', '0'))
+            extraports_state = extraports.get('state', '')
+            if extraports_state == 'closed':
+                ports_closed += extraports_count
+            elif extraports_state == 'filtered':
+                ports_filtered += extraports_count
+            ports_tested += extraports_count
+
+        host_start_time = host.get('starttime')
+        host_end_time = host.get('endtime')
+        start_timestamp = int(host_start_time) * 1000 if host_start_time else None
+        end_timestamp = int(host_end_time) * 1000 if host_end_time else None
+
+        hosts.append({
+            'ip': ip,
+            'hostname': hostname,
+            'os': os,
+            'ports_tested': ports_tested,
+            'ports_open': ports_open,
+            'ports_closed': ports_closed,
+            'ports_filtered': ports_filtered,
+            'start_time': start_timestamp,
+            'end_time': end_timestamp,
+            'ports': ports
+        })
+
+    scan = {
+        'nmap_version': nmap_version,
+        'command_line': command_line,
+        'start_time': scan_start_time,
+        'elapsed_time': elapsed_time,
+        'total_hosts': total_hosts,
+        'total_open_ports': total_open_ports
+    }
+
+    return scan, hosts
+
+def create_database(db_name):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS scans
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 nmap_version TEXT,
+                 command_line TEXT,
+                 start_time INTEGER,
+                 elapsed_time TEXT,
+                 total_hosts INTEGER,
+                 total_open_ports INTEGER)''')
+    
+    c.execute('''CREATE TABLE IF NOT EXISTS hosts
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 scan_id INTEGER,
+                 ip TEXT,
+                 hostname TEXT,
+                 os TEXT,
+                 ports_tested INTEGER,
+                 ports_open INTEGER,
+                 ports_closed INTEGER,
+                 ports_filtered INTEGER,
+                 start_time INTEGER,
+                 end_time INTEGER,
+                 FOREIGN KEY (scan_id) REFERENCES scans (id))''')
+
+    c.execute('''CREATE TABLE IF NOT EXISTS ports
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 scan_id INTEGER,
+                 host_id INTEGER,
+                 port TEXT,
+                 protocol TEXT,
+                 state TEXT,
+                 service_name TEXT,
+                 service_info TEXT,
+                 http_title TEXT,
+                 ssl_common_name TEXT,
+                 ssl_issuer TEXT,
+                 FOREIGN KEY (scan_id) REFERENCES scans (id),
+                 FOREIGN KEY (host_id) REFERENCES hosts (id))''')
+    
+    conn.commit()
+    return conn
+
+def insert_data(conn, scan, hosts):
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO scans (nmap_version, command_line, start_time, elapsed_time, total_hosts, total_open_ports) VALUES (?, ?, ?, ?, ?, ?)",
+              (scan['nmap_version'], scan['command_line'], scan['start_time'], scan['elapsed_time'], scan['total_hosts'], scan['total_open_ports']))
+    scan_id = c.lastrowid
+    
+    for host in hosts:
+        c.execute("INSERT INTO hosts (scan_id, ip, hostname, os, ports_tested, ports_open, ports_closed, ports_filtered, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                  (scan_id, host['ip'], host['hostname'], host['os'], host['ports_tested'], host['ports_open'], host['ports_closed'], host['ports_filtered'], host['start_time'], host['end_time']))
+        host_id = c.lastrowid
+        
+        for port in host['ports']:
+            c.execute("INSERT INTO ports (scan_id, host_id, port, protocol, state, service_name, service_info, http_title, ssl_common_name, ssl_issuer) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                      (scan_id, host_id, port['port'], port['protocol'], port['state'], port['service_name'], port['service_info'], port['http_title'], port['ssl_common_name'], port['ssl_issuer']))
+     
+    conn.commit()
+
+def main():
+    parser = argparse.ArgumentParser(description="Process nmap scan results.")
+    parser.add_argument("xml_file", help="Path to the nmap output XML file")
+    args = parser.parse_args()
+
+    xml_file = args.xml_file
+    db_name = 'nmap_results.db'
+
+    scan, hosts = parse_nmap_xml(xml_file)
+    conn = create_database(db_name)
+    insert_data(conn, scan, hosts)
+
+    conn.close()
+
+if __name__ == '__main__':
+    main()
